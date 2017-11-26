f0d864ca46b7dc4a366b39b064f43633c2de09e2
Sarah Lessner <sarahlessner@gmail.com>
2017-11-25 23:27:27 -0800

    code cleanup old project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 
-dogsArray = ["labrador", "husky", "rottweiler", "pitbull", "pug", "beagle", 
+dogsArray = ["labrador", "husky", "rottweiler", "pitbull", "pug", "beagle",
 "collie", "shepherd", "yorkie", "samoyed", "mastiff"];
 
-imagesArray =["assets/images/hangman10.bmp", "assets/images/hangman9.bmp", "assets/images/hangman8.bmp", 
+imagesArray =["assets/images/hangman10.bmp", "assets/images/hangman9.bmp", "assets/images/hangman8.bmp",
 "assets/images/hangman7.bmp", "assets/images/hangman6.bmp","assets/images/hangman5.bmp",
-"assets/images/hangman4.bmp", "assets/images/hangman3.bmp", "assets/images/hangman2.bmp", 
+"assets/images/hangman4.bmp", "assets/images/hangman3.bmp", "assets/images/hangman2.bmp",
 "assets/images/hangman1.bmp", "assets/images/hangman0.bmp"];
 
 dogImgArray = ["assets/images/puppy0.jpg", "assets/images/puppy01.jpg", "assets/images/puppy02.jpg",
@@ -23,8 +23,6 @@ document.getElementById("turnsleft").innerHTML = " " + turnsleft;
 document.getElementById("guesstheword").innerHTML = "Press 1 to Guess a Breed:";
 document.getElementById("wordtoguess").innerHTML = "? ? ? ? ? ? ?";
 
-
-
 document.onkeyup = function(event) {
 	document.getElementById("genword").innerHTML = "";
 
@@ -32,23 +30,18 @@ document.onkeyup = function(event) {
 
 	keyPress = keyPress.toLowerCase();
 
-	
+
 	if (randomDog === "") {
-		
+
 		if (keyPress === "1") {
 			initRound();
-			
 		}
-
 		else {
-
-		document.getElementById("genword").innerHTML = "Press 1 to Generate a New Word!!!";
-
+			document.getElementById("genword").innerHTML = "Press 1 to Generate a New Word!!!";
 		}
- 	
+
 	}
 	else {
-	
 		if (keyPress >= 'a' && keyPress <= 'z') {
 			var match = false;
 			for (var j = 0; j < randomDog.length; j++) {
@@ -56,17 +49,15 @@ document.onkeyup = function(event) {
 					hiddenDog[j] = keyPress;
 					match = true;
 				}
-				
-			}
+		}
 			document.getElementById("wordtoguess").innerHTML = " " + hiddenDog.join(" ");
 
 			if (match === false) {
-
 				if (wrongletters.includes(keyPress)){
 					document.getElementById("genword").innerHTML = "Already Guessed - Select a New Letter";
 				}
-				else { 
-					wrongletters.push(keyPress); 
+				else {
+					wrongletters.push(keyPress);
 					document.getElementById("guessed").innerHTML = " " + wrongletters.join(" ");
 					turnsleft--;
 					document.getElementById("turnsleft").innerHTML = " " + turnsleft;
@@ -76,23 +67,21 @@ document.onkeyup = function(event) {
 						document.getElementById("genword").innerHTML = "Press 1 to Generate a New Word";
 						document.getElementById("wordtoguess").innerHTML = " " + randomDog;
 						randomDog = "";
-					}	
+					}
 				}
-
 			}
-			if (hiddenDog.join("") == randomDog) {
-				wins++;
-				document.getElementById("wins").innerHTML = " " + wins;
-				document.getElementById("guesstheword").innerHTML = "Who's a good boy? "
-				document.getElementById("puppypic").src = dogImgArray[randomIdx];
-				randomDog = "";
-				document.getElementById("genword").innerHTML = "Press 1 to Generate a New Dog to Guess";
-
+				if (hiddenDog.join("") == randomDog) {
+					wins++;
+					document.getElementById("wins").innerHTML = " " + wins;
+					document.getElementById("guesstheword").innerHTML = "Who's a good boy? "
+					document.getElementById("puppypic").src = dogImgArray[randomIdx];
+					randomDog = "";
+					document.getElementById("genword").innerHTML = "Press 1 to Generate a New Dog to Guess";
+				}
 			}
-		}
-		else {
-			document.getElementById("genword").innerHTML = "Press any letter key to make a guess"; 
-		}	
+			else {
+					document.getElementById("genword").innerHTML = "Press any letter key to make a guess";
+				}
 	}
 };
 
@@ -102,23 +91,15 @@ function initRound(){
 	hiddenDog = new Array(randomDog.length);
 	wrongletters = [];
 	turnsleft = 10;
+
 	for (var i = 0; i < hiddenDog.length; i++){
 		hiddenDog[i] = "_";
-
 	}
+
 	document.getElementById("guesstheword").innerHTML = "Guess the Dog Breed by Pressing Letters:";
 	document.getElementById("wordtoguess").innerHTML = " " + hiddenDog.join(" ");
 	document.getElementById("guessed").innerHTML = " " + wrongletters.join(" ");
 	document.getElementById("turnsleft").innerHTML = " " + turnsleft;
 	document.getElementById("hangimages").src = imagesArray[turnsleft];
 	document.getElementById("puppypic").src = "";
-
 };
-
-		
-
-
-
-
-
-
